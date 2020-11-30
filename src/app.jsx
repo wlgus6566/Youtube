8085fa45ec1a675191e9a3d29b5da15cd6966314
Ji Hyeon <wlgus6566@naver.com>
2020-12-01 02:13:07 +0900

    responsive

--- src/app.jsx
+++ src/app.jsx
@@ -1,37 +1,47 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styles from './app.module.css';
+import VideoList from './components/video_list/vidio_list';
 import SearchHeader from './components/search_header/search_header';
 import VideoDetail from './components/video_detail/video_detail';
-import VideoList from './components/video_list/vidio_list';
 
-function App({youtube}) {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
-  const selectVideo = (video) => {
+
+  const selectVideo = video => {
     setSelectedVideo(video);
-  }
-  const search = query => {
-    youtube.search(query) //
-    .then(videos => {
-      setVideos(videos)
-      setSelectedVideo(null);
-    });
   };
+
+  const search = useCallback(
+    query => {
+      setSelectedVideo(null);
+      youtube
+        .search(query) //
+        .then(videos => setVideos(videos));
+    },
+    [youtube]
+  );
+
   useEffect(() => {
-    youtube.mostPopular() //
-      setVideos(videos)
-  }, []);
-  return(
+    youtube
+      .mostPopular() //
+      .then(videos => setVideos(videos));
+  }, [youtube]);
+  return (
     <div className={styles.app}>
-      <SearchHeader onSearch = {search}/>
-      <section className = {styles.content}>
-        {selectedVideo && 
-          <div className = {styles.detail}>
-            <VideoDetail video = {selectedVideo}/>
+      <SearchHeader onSearch={search} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
           </div>
-        }
+        )}
         <div className={styles.list}>
-          <VideoList videos={videos} onVideoClick = {selectVideo} display = {selectedVideo ? 'list' : 'grid'}/>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? 'list' : 'grid'}
+          />
         </div>
       </section>
     </div>
